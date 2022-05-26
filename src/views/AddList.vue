ba6dad88563472210a76aaaea8b07331ce589551
sosira <S.phairoh@gmail.com>
2022-05-26 22:35:38 +0700

    update

--- src/views/AddList.vue
+++ src/views/AddList.vue
@@ -3,8 +3,8 @@ import { ref } from 'vue'
 import NewBooking from '../components/NewBooking.vue'
 import router from '../router'
 
-// const url = 'http://intproj21.sit.kmutt.ac.th:80/ssi5/api'
-const url = '  http://202.44.9.103:8080/ssi5/api'
+const url = 'http://intproj21.sit.kmutt.ac.th:80/ssi5/api'
+// const url = '  http://202.44.9.103:8080/ssi5/api'
 
 const Categorydetails = ref([])
 
